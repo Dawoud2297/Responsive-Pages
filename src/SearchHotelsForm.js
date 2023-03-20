58f0ed75630ab06cf01e9d5a5c1ec46fc5a7211c
Dawoud2297 <soluman2297@gmail.com>
2023-03-20 19:12:43 +0200

    dynamic-options-button

--- src/SearchHotelsForm.js
+++ src/SearchHotelsForm.js
@@ -2,9 +2,10 @@ import React, { useState } from 'react'
 import DatePicker from "react-datepicker";
 
 const SearchHotelsForm = () => {
-    const [options, setOptions] = useState({ adults: 1, children: 0, rooms: 1 });
     const [dateRange, setDateRange] = useState([null, null]);
     const [checkIn, checkOut] = dateRange;
+    const [openOptions, setOpenOptions] = useState(false)
+    const [options, setOptions] = useState({ adults: 1, children: 0, rooms: 1 });
 
     const increment = (e) => {
         let name = e.target.name;
@@ -13,10 +14,6 @@ const SearchHotelsForm = () => {
         })
     }
 
-
-    const submitForm = e => {
-        e.preventDefault();
-    }
 
 
     const decrement = (e) => {
@@ -32,6 +29,20 @@ const SearchHotelsForm = () => {
                 return { ...curVal, [name]: curVal[name] - 1 }
             }
         })
+    }
+
+    const openAndClose = () => {
+        setOpenOptions(true)
+        if (openOptions) {
+            setOpenOptions(false)
+        }
+    }
+
+    const submitForm = e => {
+        e.preventDefault();
+        /*
+        dispatch the action (e.i.fetch data from the API)
+        */
     }
     return (
         <div className='formContainer'>
@@ -58,34 +69,44 @@ const SearchHotelsForm = () => {
                         onChangeRaw={e => e.preventDefault()}
                     />
                 </div>
-                <div className='options'>
-                    <div className='adultsNum category'>
-                        <h1>
-                            Adults
-                        </h1>
-                        <div className='counter'>
-                            <button type='button' name="adults" onClick={(e) => increment(e)}>+</button>
-                            <h3>{options.adults}</h3>
-                            <button type='button' name='adults' onClick={decrement}>-</button>
+                {
+                    !openOptions ? (
+                        <button type="button" className="openAndClose" onClick={openAndClose}>
+                            {options.adults} Adults , {options.children} Children, {options.rooms} Rooms
+                        </button>
+                    ) : (
+
+                        <div className='options'>
+                            <div className='adultsNum category'>
+                                <h1>
+                                    Adults
+                                </h1>
+                                <div className='counter'>
+                                    <button type='button' name="adults" onClick={(e) => increment(e)}>+</button>
+                                    <h3>{options.adults}</h3>
+                                    <button type='button' name='adults' onClick={decrement}>-</button>
+                                </div>
+                            </div>
+                            <div className='childrenNum category'>
+                                <h1>Children</h1>
+                                <div className='counter'>
+                                    <button type='button' name="children" onClick={increment}>+</button>
+                                    <h3>{options.children}</h3>
+                                    <button type='button' name='children' onClick={decrement}>-</button>
+                                </div>
+                            </div>
+                            <div className='roomNum category'>
+                                <h1>Rooms</h1>
+                                <div className='counter'>
+                                    <button type='button' name="rooms" onClick={increment}>+</button>
+                                    <h3>{options.rooms}</h3>
+                                    <button type='button' name='rooms' onClick={decrement}>-</button>
+                                </div>
+                            </div>
+                            <button type='button' className='done' onClick={openAndClose}>Done</button>
                         </div>
-                    </div>
-                    <div className='childrenNum category'>
-                        <h1>Children</h1>
-                        <div className='counter'>
-                            <button type='button' name="children" onClick={increment}>+</button>
-                            <h3>{options.children}</h3>
-                            <button type='button' name='children' onClick={decrement}>-</button>
-                        </div>
-                    </div>
-                    <div className='roomNum category'>
-                        <h1>Rooms</h1>
-                        <div className='counter'>
-                            <button type='button' name="rooms" onClick={increment}>+</button>
-                            <h3>{options.rooms}</h3>
-                            <button type='button' name='rooms' onClick={decrement}>-</button>
-                        </div>
-                    </div>
-                </div>
+                    )
+                }
                 <button type="submit" className="btn searchButton">Search </button>
             </form>
         </div>
